8a978c4d1c2e7fe661d3866f873c2b54753b2b49
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-07-28 23:12:52 -0400

    Pattern 2 code

--- DSA-1/Patterns/Pattern2.java
+++ DSA-1/Patterns/Pattern2.java
@@ -1,16 +1,18 @@
 import java.util.*;
 
-public class Pattern1 {
+public class Pattern2 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
-        int n = scn.nextInt();
-        for(int i=1; i<=n;i++){
-            for(int j=1; j<=i; j++){
+        int n  = scn.nextInt();
+
+        for(int i=n; i>=1; i--){
+            for(int j=i; j>=1; j--){
                 System.out.print("*\t");
             }
             System.out.println();
         }
+
     }
 }
