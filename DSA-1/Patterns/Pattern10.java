1a3a5739974c00e42ffa11a51f9138ca62486cf4
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-07-31 09:31:21 -0400

    Pattern 10 code

---
+++ DSA-1/Patterns/Pattern10.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Pattern10{
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        // outer space and inner space
+        
+    }
+}
