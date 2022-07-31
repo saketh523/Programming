d44a172b8e39100c015b5277219508a8e5268168
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-07-30 20:11:45 -0400

    Pattern 5 code

---
+++ DSA-1/Patterns/Pattern5.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Pattern5 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int space = n/2;
+        int star = 1;
+
+        for(int i=1; i<=n; i++){
+            // System.out.println(space + " " + star);
+            if(i <= n/2){
+                for(int j=1; j<=space; j++){
+                    System.out.print("\t");
+                }
+                for(int k=1; k<=star; k++){
+                    System.out.print("*\t");
+                }
+                space--;
+                star = star+2;
+            }
+            else{
+                for(int j=1; j<=space; j++){
+                    System.out.print("\t");
+                }
+                for(int k=1; k<=star; k++){
+                    System.out.print("*\t");
+                }
+                space++;
+                star = star-2;
+            }
+            System.out.println();
+        }
+
+
+    }
+}
