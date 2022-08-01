bc467f8961a2510c346431913ca6ea8c8b0ae05a
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-07-31 21:13:24 -0400

    Pattern 20 code

--- DSA-1/Patterns/Pattern18.java
+++ DSA-1/Patterns/Pattern18.java
@@ -1,34 +1,49 @@
 import java.util.*;
 
-public class Pattern18{
+public class Pattern19{
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
 
         int n = scn.nextInt();
-        int star = n;
-        int ospace = 0;
 
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=ospace; j++){
-                System.out.print("\t");
-            }
-            for(int j=1; j<=star; j++){
-                if(i > 1 && i <= n/2 && j >1 && j < star){
-                    System.out.print("\t");
+            for(int j=1; j<=n; j++){
+                if(i == 1){
+                    if(j==n || j <= n/2 +1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
                 }
-                else{
+                else if(i <= n/2 ){
+                    if(j== n/2 + 1 || j==n){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+                else if(i == n/2 + 1){
                     System.out.print("*\t");
                 }
-
-            }
-            if(i <=n/2){
-                star = star - 2;
-                ospace++;
-            }
-            else{
-                star = star + 2;
-                ospace--;
+                else if(i < n){
+                    if(j==1 || j== n/2 +1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
+                else{
+                    if(j==1 || j>= n/2 +1){
+                        System.out.print("*\t");
+                    }
+                    else{
+                        System.out.print("\t");
+                    }
+                }
             }
             System.out.println();
         }
