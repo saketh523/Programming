e43d13440d57f85eb5bb347b356941a650940c62
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-07-31 21:14:08 -0400

    Pattern 18 code

--- DSA-1/Patterns/Pattern18.java
+++ DSA-1/Patterns/Pattern18.java
@@ -4,7 +4,6 @@ public class Pattern19{
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
 
         for(int i=1; i<=n; i++){
