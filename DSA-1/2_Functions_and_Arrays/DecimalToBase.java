3710bb785b3245f983121c8498e88e334f6a7ef8
SakethJammula <89420665+SakethJammula@users.noreply.github.com>
2022-08-01 21:22:42 -0400

    DecimalToBase Problem

---
+++ DSA-1/2_Functions_and_Arrays/DecimalToBase.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class DecimalToBase{
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b = scn.nextInt();
+        int dn = getValueInBase(n, b);
+        System.out.println(dn);
+    }
+
+    public static int getValueInBase(int n, int b){
+        int rv = 0;
+        int p = 1;
+
+        while(n>0){
+            int dig = n%b;
+            n = n/b;
+
+            rv = rv + dig * p;
+            p = p * 10;
+        }
+
+        return rv;
+
+    }
+}
